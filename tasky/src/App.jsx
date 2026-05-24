c9f5625c5834cee829f0dcc551a349c03209de99
Lauren <laurenaoife.mur@gmail.com>
2026-05-24 21:22:05 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -21,6 +21,12 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+    const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
 
     return (
     <div className="container">
@@ -34,6 +40,8 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+
 
     />
   ))} 
